181c0a17775aea88c27c0c531e8b33600eeac87d
Devday <Devday@BRRIOWN022373.stone.local>
2019-10-10 19:31:33 -0300

    mockagem

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var md5 = require('md5');
+var consts = require('consts');
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-    const token = md5('ghfyfuyfiyf')
+    const token = md5('ghfyfuyfiyf');
   res.json({
       token: token
     });
--- routes/transaction.js
+++ routes/transaction.js
@@ -1,19 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var bdtrans =[
 
-router.post('/', function(req, res, next) {
+];
+var id = 0;
+
+router.post('/', function(req, res, next) { 
+    id += 1;
     var body = req.body
-    
+    let trans = {
+        index : id,
+        trans : body
+    }
+    bdtrans.push(trans);
+  
   res.json({
-      message: "transação criada com sucesso"
+      message: "/api/v1/transaction/"+id,
     });
 });
 
 router.post('/:transaction_id', function(req, res, next) {
-    var transaction = req.param.transaction_id;
-    res.json({
-        message: "pagamento confirmado"
-      });
+    var transaction = req.params.transaction_id;
+    var trans = null;
+    bdtrans.forEach(x =>{
+        if (x.index == transaction)
+            trans = x.trans;
+        });
+    
+    if (trans != null){ 
+        
+        res.json({
+            capture: {
+                api_key: trans.api_key,
+                capture: true,
+                ammount: trans.ammount
+            }
+        });
+    }
   });
+  
 
 module.exports = router;
